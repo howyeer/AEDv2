de8d04bb74c725d1cd54fc7c5a3276aca119b5da
howyeer <howyeer32@qq.com>
2025-03-17 10:50:16 +0800

    3.17inferval

---
+++ aedv2/config/tao_inference.sh
@@ -0,0 +1,5 @@
+python aedv2/inference_tao.py\
+    --exp_name "infer_result"\
+    --split "test"\
+    -c "aedv2/config/GroundingDINO_SwinT_OGC_AED.py"\
+    -p  "aedv2/output_dir/train_result/checkpoint_005.pth"
